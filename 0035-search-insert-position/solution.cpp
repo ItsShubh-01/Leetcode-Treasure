84a1f55852ac9d9c6fba109ef68a6a8dabcf863d
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-02-23 17:05:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.6 MB (78.77%)

--- 0035-search-insert-position/solution.cpp
+++ 0035-search-insert-position/solution.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        for(int i=0 ; i<nums.size() ; i++)
-        {
-            if(nums[i]>=target && i!=0 && nums[i-1]<target)
-                return i;
-            else if(i==0 && nums[i]>=target)
-                return 0;
-            else if(i==nums.size()-1)
-                return i+1;
-        }
-        return -1;
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+
+        return it-nums.begin();
     }
 };

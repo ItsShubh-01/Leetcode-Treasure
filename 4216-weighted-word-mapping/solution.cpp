c2e612093473ed442bb2071a811f40900dbe5426
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-02-24 18:59:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (57.79%)

---
+++ 4216-weighted-word-mapping/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) 
+    {
+        string ans;
+        for(string s:words)
+        {
+            int weight = 0;
+            for(int i=0; i<s.size(); i++)
+                weight+=weights[s[i]-'a'];
+            
+            weight%=26;
+            ans+='z'-weight;
+        }
+        return ans;
+    }
+};

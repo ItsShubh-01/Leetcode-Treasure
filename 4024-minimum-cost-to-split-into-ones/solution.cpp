8cff6a3a4c4859bd3ca83407d8315e8297011723
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-03-01 02:45:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (89.50%)

---
+++ 4024-minimum-cost-to-split-into-ones/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCost(int n)
+    {
+        if(n<=1)
+            return 0;
+        if(n==2)
+            return 1;
+        return n-1 + minCost(n-1);
+    }
+};

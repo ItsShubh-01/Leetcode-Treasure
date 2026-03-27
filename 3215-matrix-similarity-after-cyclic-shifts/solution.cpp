f0c7184565590482bf71e40c1584cdf9fa93040c
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-03-27 03:39:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.1 MB (98.39%)

---
+++ 3215-matrix-similarity-after-cyclic-shifts/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                if((i%2==0 && mat[i][j]!=mat[i][(j-k%m+m)%m]) || 
+                        (i%2==1 && mat[i][j]!=mat[i][abs((j+k)%m)]))
+                    return false;
+        return true;
+    }
+};

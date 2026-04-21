0d366f7009b08d30c0fbb2b1f0b39592887966f4
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-04-21 06:39:34 +0000

    Sync LeetCode submission Runtime - 3 ms (18.09%), Memory - 8.6 MB (14.96%)

--- 0069-sqrtx/solution.cpp
+++ 0069-sqrtx/solution.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0) return 0;
-        int ans;
-        int start = 1;
-        int end = x;
-        int mid = start+(end - start)/2;
-
-        while(start<=end)
-        {
-            if(mid<=x/mid){
-                ans = mid;
-                start = mid+1;
-            }
-            else if(mid>=x/mid)
-                end = mid-1;
-            
-            mid = start+(end - start)/2;
-        }
-        return ans;
+        return sqrt(x);
     }
 };

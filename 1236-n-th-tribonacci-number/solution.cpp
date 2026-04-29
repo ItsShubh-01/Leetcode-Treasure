37cd244aafc27e74dffe243650b23a21ad02ba75
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-04-29 17:25:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (24.24%)

---
+++ 1236-n-th-tribonacci-number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    unordered_map<int,int> hash;
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        else if(n==2) return 1;
+        if(hash.find(n)!=hash.end()) return hash[n];
+        hash[n] = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        return hash[n];
+    }
+};

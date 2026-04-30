37079dff34ec54d23cfe524abf34ce810803a803
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-04-30 18:16:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (99.23%)

--- 3019-furthest-point-from-origin/solution.cpp
+++ 3019-furthest-point-from-origin/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int count = 0;
-        int dist = 0;
-        for(int i = 0;i<moves.size();i++){
-            if(moves[i]=='L') dist--;
-            else if(moves[i]=='R') dist++;
-            else count++;
+        int countL=0, countR=0, count_=0;
+        for(int i=0; i<moves.size(); i++)
+        {
+            if(moves[i]=='L') countL++;
+            else if(moves[i]=='R') countR++;
+            else count_++;
         }
-        if(dist>=0) dist+=count;
-        else dist-=count;
-        return abs(dist);
+
+        if(countL>=countR) return countL-countR+count_;
+        else return countR-countL+count_;
     }
 };

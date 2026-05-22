55367a8fe0c9c5f07b202a9a602c09129a4a3fa0
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-05-22 15:26:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.2 MB (36.98%)

--- 0033-search-in-rotated-sorted-array/solution.cpp
+++ 0033-search-in-rotated-sorted-array/solution.cpp
@@ -10,26 +10,24 @@ public:
         while(start <= end)
         {
             int mid = start + (end - start)/2;
-
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[start]<=target)
-            {
-                if(nums[mid]<nums[start])
-                    end = mid -1;
-                else if(nums[mid]<target)
-                    start = mid+1;
-                else
-                    end = mid -1;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>=nums[0]){
+                if(target>nums[mid])
+                    start = mid + 1;
+                else{
+                    if(target<nums[0])
+                        start = mid+1;
+                    else end = mid - 1;
+                }
             }
-            else
-            {
-                if(nums[mid]>nums[end])
-                    start = mid +1;
-                else if(nums[mid]>target)
-                    end = mid -1;
-                else
-                    start = mid +1;
+            else{
+                if(target<nums[mid])
+                    end = mid-1;
+                else{
+                    if(target>=nums[0])
+                        end = mid-1;
+                    else start = mid+1;
+                }
             }
         }
         return -1;

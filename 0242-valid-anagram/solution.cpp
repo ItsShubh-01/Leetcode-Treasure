585e9f81e4788bf7578ebbc825bf32fe5675bb92
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-05-25 10:30:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (49.98%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> count(26,0);
+        for(char ch:s) count[ch-'a']++;
+        for(char ch:t) count[ch-'a']--;
+        for(int i:count) if(i!=0) return false;
+        return true;
+    }
+};

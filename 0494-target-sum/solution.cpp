4db2f97218ae8b70d8a2f17768b569a6fdada6f8
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-06-25 19:59:25 +0000

    Sync LeetCode submission Runtime - 15 ms (49.90%), Memory - 25.9 MB (22.07%)

--- 0494-target-sum/solution.cpp
+++ 0494-target-sum/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int ind, int sum, int& target)
-    {
-        if(ind==nums.size())
-            return sum==target? 1:0;
+    int offset=0;
+    int f(int ind, int target, vector<int>& arr, vector<vector<int>>& dp){
+        if(ind==arr.size()) return target==0?1:0;
+        if(target<-offset || target>offset) return 0;
+        if(dp[ind][target+offset]!=-1) return dp[ind][target+offset];
 
-        sum+=nums[ind];
-        int l = f(nums, ind+1, sum, target);
-        sum-=2*nums[ind];
-        int r = f(nums, ind+1, sum, target);
-
-        return l+r;
+        int add = f(ind+1, target-arr[ind], arr, dp);
+        int subt = f(ind+1, target+arr[ind], arr, dp);
+        return dp[ind][target+offset]=add+subt;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums, 0, 0, target);
+        int sum=0; for(int& i:nums) sum+=abs(i); offset=sum;
+        vector<vector<int>> dp(nums.size(), vector<int>((2*sum)+1, -1));
+        return f(0, target, nums, dp);
     }
 };

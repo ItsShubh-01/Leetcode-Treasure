ba7b7ea16fd1a73479542b4038f5b05082150946
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-06-30 21:36:16 +0000

    Sync LeetCode submission Runtime - 123 ms (27.28%), Memory - 108.5 MB (18.87%)

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int f(int ind, int holding, int& fee, vector<int>& arr, vector<vector<int>>& dp){
+        if(ind==arr.size()) return 0;
+        if(dp[ind][holding]!=-1) return dp[ind][holding];
+
+        int do_nothing = f(ind+1, holding, fee, arr, dp);
+        int do_something = 0;
+        if(holding) do_something = arr[ind]+f(ind+1, 0, fee, arr, dp);
+        else do_something = -fee-arr[ind]+f(ind+1, 1, fee, arr, dp);
+
+        return dp[ind][holding]=max(do_nothing, do_something);
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return f(0, 0, fee, prices, dp);
+    }
+};

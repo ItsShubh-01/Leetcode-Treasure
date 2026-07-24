de671a38f5786e7b2542a4e3c65ee4df087af294
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-07-24 16:26:50 +0000

    Sync LeetCode submission Runtime - 16 ms (9.87%), Memory - 44.9 MB (12.45%)

---
+++ 0455-assign-cookies/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int count=0;
+        int i=g.size()-1, j=s.size()-1;
+        while(i>=0 && j>=0){
+            if(g[i]<=s[j]) {
+                count++;
+                i--; j--;
+            }
+            else i--;
+        }
+        return count;
+    }
+};

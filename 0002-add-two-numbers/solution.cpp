711fa8559efb4d6e0f54299cf8b5c4e589fff069
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-08-03 13:04:42 +0000

    Sync LeetCode submission Runtime - 4 ms (21.17%), Memory - 77.1 MB (76.40%)

--- 0002-add-two-numbers/solution.cpp
+++ 0002-add-two-numbers/solution.cpp
@@ -16,21 +16,17 @@ public:
         ListNode* dummy = new ListNode(0);
         ListNode* curr = dummy;
         int carry = 0;
-        while(l1 || l2)
-        {
+        while(l1 || l2) {
             int sum = carry;
-            if(l1)
-            {
+            if(l1){
                 sum+=l1->val;
                 l1 = l1->next;
             }
-            if(l2)
-            {
+            if(l2) {
                 sum+=l2->val;
                 l2 = l2->next;
             }
             carry = sum/10;
-
             curr->next = new ListNode(sum%10);
             curr = curr->next;
         }

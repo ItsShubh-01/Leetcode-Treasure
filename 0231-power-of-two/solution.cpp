fa25ecc31c1460d53b1bde69d26749579438c6ea
Shubh Agarwal <shubhagarwalaaaaa@gmail.com>
2026-08-12 17:20:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (50.51%)

--- 0231-power-of-two/solution.cpp
+++ 0231-power-of-two/solution.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i=0; i<31; i++)
-            if(n==(1<<i))
-                return true;
-        return false;
+        return n>0 && (n&(n-1))==0;
     }
 };
